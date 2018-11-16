1a2049da7fe256d282bbfb09ff124bc6970ab8bd
RhedOrca <33771009+RhedOrca@users.noreply.github.com>
2018-11-16 13:03:41 -0800

    Updated to maintain sorting in lists.
    use arrayMergerSorted to merge sorted lists. Throws error if A.length is not appropriately sized for both lists.

--- arrayMerger.java
+++ arrayMerger.java
@@ -3,7 +3,7 @@ package challenges;
 import java.util.*;
 
 class Merger {
-	public static void arrayMerger(int[] A, int[] B) {
+	public static void arrayMergerNoSort(int[] A, int[] B) {
 		int j=0;
 		for (int i = 0; i < A.length; i++) {
 			if(A[i] == 0) {
@@ -16,12 +16,31 @@ class Merger {
 			}
 		}
 	}
+	public static void arrayMergerSorted(int[] A, int[] B, int Alength) {
+		if (A.length != Alength + B.length) throw new IllegalArgumentException();
+		int WriteTo=A.length-1;
+		int j=B.length-1;
+		for (int i = A.length-1; i >= 0; i--) {
+			if(A[i] == 0) { continue;}
+			while (j >= 0) {
+				if (A[i] >= B[j]) {
+					A[WriteTo] = A[i];
+					WriteTo--;
+					break;
+				}
+					A[WriteTo] = B[j];
+					WriteTo--;
+					j--;
+			}
+		}
+		System.out.println(Arrays.toString(A));
+	}
 }
 public class Main {
 
 	public static void main(String[] args) {
-		int[] listA = new int[] {1,2,3,0,0,0,0};
-		int[] listB = new int[] {4,5,6};
-		Merger.arrayMerger(listA, listB);
+		int[] listA = new int[] {1,3,5,0,0,0,0};
+		int[] listB = new int[] {2,4,6,8};
+		Merger.arrayMergerSorted(listA, listB, 3);
 	}	
 }
